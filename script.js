f8c195dd798063e861a0c34d7d1181aa089122cf
Xojiakbartemurboyev <notxoji@gmail.com>
2025-05-23 14:59:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let name = prompt("Ismingizni kiriting:");
+
+
+
+let harf = prompt("Bironta harf kiriting:");
+
+
+if(name.toLowerCase().includes(harf.toLowerCase())){
+    alert(`Siz kiritgan ${harf} ${name} ismida bor`)
+}
+else{
+}
+
+    alert(`Siz kiritgan ${harf} ${name} ismida mavjud emas`)
